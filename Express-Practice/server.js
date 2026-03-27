449a34656183c4ca60d02c190b315e288edfbf3b
Krunal Rathod <rathod.krunal.rk@gmail.com>
2026-03-27 22:11:12 +0530

    save

--- Express-Practice/server.js
+++ Express-Practice/server.js
@@ -4,7 +4,24 @@ const app = express();
 app.get('/', (req, res) => {
     res.send('hello bhai express chal gya')
 });
+app.get('/about',(req, res) =>{
+    res.send('this is about page')
+});
+
+app.post('/user', (req ,res) => {
+    res.send('this is post user')
+});
+
+app.put('/user', (req, res) =>{
+    res.send('this is put')
+});
+
+app.delete('/delete',(req, res) =>{
+res.send('this is delete')
+});
+
 
 app.listen(3000, () =>{
     console.log('server running on port 3000 ')
 });
+
