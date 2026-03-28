62110d199e1fb07d5aa4e8a6116a206fc7b8c1e0
Krunal Rathod <rathod.krunal.rk@gmail.com>
2026-03-28 22:53:03 +0530

    save

--- Express-Practice/Middleware.js
+++ Express-Practice/Middleware.js
@@ -1,12 +1,27 @@
-const express = require('express');
-const app = express();
+// const express = require('express');
+// const app = express();
 
-//Middleware
-app.use((req, res, next) => {
-    console.log('Middleware chala');
-    next(); // very importent
-});
-app.get('/', (req, res) =>{
-    res.send('Home page')
-});
-app.listen(3000);
+// //Middleware
+// app.use((req, res, next) => {
+//     console.log('Middleware chala');
+//     next(); // very importent
+// });
+// app.get('/', (req, res) =>{
+//     res.send('Home page')
+// });
+// app.listen(3000);
+
+
+
+const checkUser = (req, res, next) =>{
+
+    const isLogin = true;
+
+    if(isLogin){
+        next();
+         }
+         else{
+            res.send('Login reqired')
+         }
+};
+app.get()
