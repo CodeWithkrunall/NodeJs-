a7383bddca235275d30ea2244c3bf314afa00b33
Krunal Rathod <rathod.krunal.rk@gmail.com>
2026-03-30 23:31:02 +0530

    save

--- Express-Practice/Request&Response.js
+++ Express-Practice/Request&Response.js
@@ -4,3 +4,11 @@ app.get('/user/.id', (req, res) =>{
     const userId = req.params.id;
     res.send(`user id is${userId}`);
 });
+
+// . req.query (URL ke ? se data lena)
+
+app.get('/search', (req,res) => {
+    const name = req.query.name;
+    res.send(`serching for ${name}`);
+});
+
