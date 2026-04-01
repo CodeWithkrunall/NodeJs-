d02db68b794ef811e2713fe54bb4dc190119e6fb
Krunal Rathod <rathod.krunal.rk@gmail.com>
2026-04-01 19:11:05 +0530

    sa e

---
+++ Module-Basics/Node-module/math.js
@@ -0,0 +1,18 @@
+/*function add(a, b){
+    return a+ b;
+}
+module.exports = add;
+
+*/
+
+//multiple functions expoet
+
+function sub(a, b){
+    return a-b;
+};
+function cul(a, b){
+    return a*b;
+};
+
+module.exports = {sub, cul};
+
