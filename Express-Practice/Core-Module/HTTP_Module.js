02ec8d3a77cac29d4401c3b9e3bf13c4783be52e
Krunal Rathod <rathod.krunal.rk@gmail.com>
2026-04-01 22:50:10 +0530

    save

--- Express-Practice/Core-Module/HTTP_Module.js
+++ Express-Practice/Core-Module/HTTP_Module.js
@@ -1,4 +1,4 @@
-const http = require('http');
+/*const http = require('http');
 
 //create a server object
 
@@ -9,4 +9,15 @@ const server = http.createServer((req,res) => {
 
 server.listen(3000, ()=>{
     console.log('server running on port 3000');
+});
+*/
+const http = require('http');
+
+const server = http.createServer((req, res) =>{
+    res.writeHead(200, {'contect type ':'text plan'});
+    res.end('hello world')
+})
+
+Server.listen(3000, ()=>{
+    console.log(`this server is running in 300 poer`)
 });
