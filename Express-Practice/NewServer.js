7d56e378521ab82b47fac5520e9376071411068d
Krunal Rathod <rathod.krunal.rk@gmail.com>
2026-04-21 22:56:48 +0530

    save

---
+++ Express-Practice/NewServer.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+
+const port = 3000;
+
+app.get('/', (req,res)=>{
+    res.send('this is server portal')
+});
+
+app.listen(port , () =>{
+
+    console.log(` this server run on ${port}`)
+
+
+});
