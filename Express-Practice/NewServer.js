4d3ea3c2b590a6df9933ba09c4573c39cf1bc61d
Krunal Rathod <rathod.krunal.rk@gmail.com>
2026-04-22 06:43:30 +0530

    save

--- Express-Practice/NewServer.js
+++ Express-Practice/NewServer.js
@@ -2,11 +2,23 @@ const express = require('express');
 const app = express();
 
 const port = 3000;
-
+// router
 app.get('/', (req,res)=>{
     res.send('this is server portal')
 });
+
+// params
+app.get('/user/:username', (req, res)=>{
+    const username = req.params.username
+    res.send(`welcome ${Krunal}`);
+});
+
+//quiry   // search?keyword=express
+app.get('/search', (req, res)=>{
+    const keyword = req.query.keyword
+    res.send( ` search .... keyword is ${keyword}` )
+}
+
 app.listen(port , () =>{
     console.log(` this server run on ${port}`)
-
 });
