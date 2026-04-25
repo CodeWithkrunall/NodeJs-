ad52b17cf4f9c40e2efee1cae0f6834eb8b3bafd
Krunal Rathod <rathod.krunal.rk@gmail.com>
2026-04-25 15:46:00 +0530

    save

--- Express-Practice/index.js
+++ Express-Practice/index.js
@@ -1,11 +1,20 @@
-import express from 'express'
+
+const express= require('express')
 
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 const PORT =3000
 
 app.get('/', (req,res)=>{
     res.send('express ')
+})
+
+app.post('/form', (req,res) =>{
+    console.log(req.body)
+    res.send('Form Received')
 })
 
 app.listen(PORT, ()=>{
