6adddb4095eb570764a3bc31c52ada6afb92b4c6
Krunal Rathod <rathod.krunal.rk@gmail.com>
2026-04-25 12:13:10 +0530

    save

---
+++ Express-Practice/index.js
@@ -0,0 +1,13 @@
+import express from 'express'
+
+const app = express();
+
+const PORT =3000
+
+app.get('/', (req,res)=>{
+    res.send('express ')
+})
+
+app.listen(PORT, ()=>{
+    console.log(`server is running on http:/localhost:${PORT}`)
+})
