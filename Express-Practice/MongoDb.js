b91ae99db3d7ede6759faa6c3e82cd22413d4709
Krunal Rathod <rathod.krunal.rk@gmail.com>
2026-04-26 13:32:47 +0530

    sace

--- Express-Practice/MongoDb.js
+++ Express-Practice/MongoDb.js
@@ -1,15 +1,46 @@
-const express = require('express')
 
-const app = express();
+// const express = require('express')
 
-const PORT = 3000;
 
-const MONGODB_URL ='mongodb+srv://KrunalStack:<db_password>@krunal.pnwwzw7.mongodb.net/?appName=krunal'
+// const app = express();
+// const PORT = 3000;
 
-app.get('/', (req, res)=>{
-    res.send('express start')
-});
+// const MONGODB_URL ="mongodb+srv://KrunalStack:krunal123@cluster0.o1bonfu.mongodb.net/?appName=Cluster0"
 
-app.listen(PORT, () =>{
-    console.log(`server is running on http:/localhost:${PORT}`)
-})
+// mongoose.connect(MONGODB_URL).then(()=>{
+//    console.log('database connected') 
+// })
+
+// app.get('/', (req, res)=>{
+//     res.send('express start')
+// });
+
+// app.listen(PORT, () =>{
+//     console.log(`server is running on http:/localhost:${PORT}`)
+// })
+
+import {MongoClient} from "mongodb"
+import dns from "dns"
+
+dns.setServers(["1.1.1.1", "8.8.8.8"]);
+//mongodb atlas connection string
+
+const url = "mongodb+srv://KrunalStack:kru123@cluster0.o1bonfu.mongodb.net/database?appName=Cluster0"
+
+const client = new MongoClient(url);
+
+//db name
+
+const dbName = 'database' 
+
+async function main(){
+    try{
+        //connect to atlas
+        await client.connect();
+        console.log("connect to mongodb")
+    }catch(error){
+        console.error(error);
+
+    }
+}
+main();
